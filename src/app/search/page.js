bfbae6bc91db4235e66e10ebfc849ec842aaafb1
unknown <fatihalsena@gmail.com>
2024-06-02 03:16:44 +0700

    masih tahap awal

---
+++ src/app/search/page.js
@@ -0,0 +1,14 @@
+// export default function Page() {
+//     return <div>SEARCH PAGE</div>
+// }
+
+const Page = () => {
+    return (
+        <h1>
+            <div>SEARCH PAGE</div>
+            <p>ini adalah search page</p>
+        </h1>
+    )
+}
+
+export default Page
